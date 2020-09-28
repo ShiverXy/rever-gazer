11216dfb817f98d9d9eae0c70c782f5da9dbd289
Shiver <29890931+ShiverXy@users.noreply.github.com>
2020-09-28 11:15:26 +0100

    current upload

---
+++ res/scpt/automain.gd
@@ -0,0 +1,15 @@
+extends Control
+
+func _ready():
+	var nickname  = 0
+	var species = 0
+	var gender = 0
+	var nature = 0
+	#World globals
+	var wrld = 1
+	var hour = 0
+	var tmin = 0
+	var area = 0
+	var zone = 0
+	var temp = 0
+	var step = 0
