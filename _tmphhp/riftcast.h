0e4919bfd357fa0b1fb9d0d4894197aa166f5ece
ShiverXy <29890931+ShiverXy@users.noreply.github.com>
2020-11-10 18:51:47 +0000

    Executable

---
+++ _tmphhp/riftcast.h
@@ -0,0 +1,13 @@
+#define welcome 0
+#define versionlistings 0
+#define controls 0
+#define prexp 0
+#define XPandNewer 0
+#define changelog 0
+#define knownissues 0
+#define training 0
+#define cleaning 0
+#define adventures 0
+#define Credits 0
+#define Contact 0
+
